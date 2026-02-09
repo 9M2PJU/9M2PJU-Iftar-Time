90e35e5e194be9a340fc6bb7660813fd4466c142
9M2PJU <9m2pju@gmail.com>
2026-02-09 16:10:58 +0800

    style: fix navbar height collapse by switching to flex centering

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 export const Navbar: React.FC = () => {
     return (
-        <nav className="w-full p-4 flex items-center justify-between z-50 relative">
+        <nav className="w-full p-4 flex items-center justify-center z-50 relative">
             {/* Centered Title with Logo */}
-            <div className="flex absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex-col items-center w-full">
+            <div className="flex flex-col items-center w-full">
                 <div className="flex items-center gap-3">
                     <div className="w-8 h-8 bg-emerald-500 rounded-lg flex items-center justify-center shadow-lg shadow-emerald-500/20">
                         <svg viewBox="0 0 24 24" fill="none" className="w-5 h-5 text-white" stroke="currentColor" strokeWidth="2">
